bbcc1fa98ae24d3529793c6e0c072edfc5e9ad0b
Challa <jchalla@medline.com>
2020-04-13 02:19:41 -0500

    initial commit

---
+++ ratings-data-service/src/main/java/com/med/springboot/ratingsdataservice/RatingsDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.med.springboot.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsDataServiceApplication.class, args);
+	}
+
+}
---
+++ ratings-data-service/src/test/java/com/med/springboot/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.springboot.ratingsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
