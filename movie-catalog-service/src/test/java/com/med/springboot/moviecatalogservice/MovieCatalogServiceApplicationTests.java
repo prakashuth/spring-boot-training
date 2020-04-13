b1ddfd13d6f12a930dd8f7e4ab41f6eee4407745
Challa <jchalla@medline.com>
2020-04-13 02:18:11 -0500

    first commit

---
+++ movie-catalog-service/src/test/java/com/med/springboot/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.springboot.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
