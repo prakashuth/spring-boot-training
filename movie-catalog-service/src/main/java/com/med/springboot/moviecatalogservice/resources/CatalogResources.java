8fd861454156fce0f46a7291e7645d74744e0240
jchalla <jchalla@L43521.medline.com>
2020-05-01 12:26:37 -0500

    Circuit Breaker With Hystrix

--- movie-catalog-service/src/main/java/com/med/springboot/moviecatalogservice/resources/CatalogResources.java
+++ movie-catalog-service/src/main/java/com/med/springboot/moviecatalogservice/resources/CatalogResources.java
@@ -10,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.med.springboot.moviecatalogservice.models.CatalogItem;
 import com.med.springboot.moviecatalogservice.models.MovieInfo;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/catalog")
@@ -20,12 +21,22 @@ public class CatalogResources {
 	RestTemplate restTemplate;
 	
 	@RequestMapping("/{userId}")
-	List<CatalogItem> getCatalog(String userId){
-		
-		MovieInfo movieInfo = restTemplate.getForObject("http://MOVIE-INFO-SERVICE/movie/100",MovieInfo.class);
+	@HystrixCommand(fallbackMethod = "getFallbackCatalog")
+		List<CatalogItem> getCatalog(String userId){
+		MovieInfo movieInfo = getMovieInfo();
 		return Collections.singletonList(new CatalogItem(movieInfo.getName(),"Transformers description",5));
 	}
+
+
+	private MovieInfo getMovieInfo() {
+		MovieInfo movieInfo = restTemplate.getForObject("http://MOVIE-INFO-SERVICE/movie/100",MovieInfo.class);
+		return movieInfo;
+	}
 	
+	
+	List<CatalogItem> getFallbackCatalog(String userId){
+		return Collections.singletonList(new CatalogItem("falback name","falback Transformers description",5));
+	}
 	
 	/*
 	Alternative WebClient way
